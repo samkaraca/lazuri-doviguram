0bc91217213db2a97d72f38396ff2db711f4955f
Hikmet <ka555861@gmail.com>
2023-07-19 11:26:25 +0300

    bugfix

--- src/core/utils/dynamo_db_client_singleton.ts
+++ src/core/utils/dynamo_db_client_singleton.ts
@@ -9,6 +9,10 @@ export class DynamoDBClientSingleton {
     if (!DynamoDBClientSingleton.instance) {
       DynamoDBClientSingleton.instance = new DynamoDBClient({
         region: "eu-west-2",
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
       });
     }
 
