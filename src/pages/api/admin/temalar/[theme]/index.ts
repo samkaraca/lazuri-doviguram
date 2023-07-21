cb52f886e480b6d05e7c7053482aecae947ab9ee
Hikmet <ka555861@gmail.com>
2023-07-21 18:33:42 +0300

    bugfix

--- src/pages/api/admin/temalar/[theme]/index.ts
+++ src/pages/api/admin/temalar/[theme]/index.ts
@@ -28,8 +28,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     return res.status(501).json({ error: "Unsopported action" });
   } else if (req.method === "GET") {
+    console.warn("/temalar/[theme] request made");
     const theme = req.query.theme as string;
     const themeData = await themeRepo.getThemeData(theme);
+    console.warn("/temalar/[theme] request response received", themeData);
     return res.status(200).json(themeData);
   }
 
